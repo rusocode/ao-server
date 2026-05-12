9890ca152a639760a904f835f9a3409b8ac001de
rusocode <juandebenedetti94@gmail.com>
2026-05-12 18:37:54 -0300

    refactor: remove unused imports from PingPacket and redundant byte guard from WalkPacket

--- server/src/main/java/com/ao/network/packet/incoming/PingPacket.java
+++ server/src/main/java/com/ao/network/packet/incoming/PingPacket.java
@@ -1,19 +1,15 @@
 package com.ao.network.packet.incoming;
 
-import com.ao.model.fonts.Font;
 import com.ao.network.Connection;
 import com.ao.network.DataBuffer;
 import com.ao.network.packet.IncomingPacket;
-import com.ao.network.packet.outgoing.ConsoleMessagePacket;
 import com.ao.network.packet.outgoing.PongPacket;
 
 public class PingPacket implements IncomingPacket {
 
     @Override
     public boolean handle(DataBuffer buffer, Connection connection) {
-
         connection.send(new PongPacket());
-
         return true;
     }
 
