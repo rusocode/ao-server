563cd1ace6779229fbf31d6a4796561ed7a362be
rusocode <juandebenedetti94@gmail.com>
2026-05-13 16:04:26 -0300

    test: improve MapServiceImpl tests with meaningful assertions and out-of-range coverage

--- server/src/test/java/com/ao/service/map/MapServiceImplTest.java
+++ server/src/test/java/com/ao/service/map/MapServiceImplTest.java
@@ -15,12 +15,33 @@ import static org.mockito.Mockito.when;
 public class MapServiceImplTest {
 
     @Test
-    public void testLoadMaps() {
+    public void testLoadMaps_mapsAccessibleAfterLoad() {
+        Map map1 = new Map(null, 1, (short) 1, new Tile[]{}, 0, false, false, "BOSQUE", "CAMPO", false, 0, false);
+        Map map2 = new Map(null, 2, (short) 1, new Tile[]{}, 0, false, false, "CIUDAD", "CAMPO", false, 0, false);
         MapDAO dao = mock(MapDAO.class);
         CityDAO cityDao = mock(CityDAO.class);
         AreaService areaService = mock(AreaService.class);
+        when(dao.load()).thenReturn(new Map[]{map1, map2});
+
         MapService service = new MapServiceImpl(dao, cityDao, areaService);
         service.loadMaps();
+
+        assertThat(service.getMap(1)).isSameAs(map1);
+        assertThat(service.getMap(2)).isSameAs(map2);
+    }
+
+    @Test
+    public void testLoadMaps_outOfRangeIdReturnsNull() {
+        MapDAO dao = mock(MapDAO.class);
+        CityDAO cityDao = mock(CityDAO.class);
+        AreaService areaService = mock(AreaService.class);
+        when(dao.load()).thenReturn(new Map[]{});
+
+        MapService service = new MapServiceImpl(dao, cityDao, areaService);
+        service.loadMaps();
+
+        assertThat(service.getMap(0)).isNull();
+        assertThat(service.getMap(99)).isNull();
     }
 
     @Test
